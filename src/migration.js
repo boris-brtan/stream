23de294c1451925ac0f5caf465be3f53490ae1a1
Boris Brtáň <boris.brtan.ponuky@gmail.com>
2022-09-01 18:10:21 +0200

    migration is enhanced

--- src/migration.js
+++ src/migration.js
@@ -1,17 +1,19 @@
 export const version = 1
 
 export function migrate() {
-    if (localStorage.streamList || localStorage.stream === undefined) {
-        localStorage.stream = JSON.stringify({
-            items: JSON.parse(localStorage.streamList).map?.(i => ({
-                title: i,
-                url: i,
-            })) || [],
-            version
-        })
+    let items = []
+    if (localStorage.streamList) {
+        try {
+            items = JSON.parse(localStorage.streamList).map(i => ({ title: i, url: i }))
+        } catch (e) {
+            console.debug('wrong format of streamList', e)
+        }
         localStorage.removeItem('streamList')
     }
+    if (localStorage.stream === undefined) {
+        localStorage.stream = JSON.stringify({ items, version })
+    }
     if (version > localStorage.stream?.version) {
-        console.log('migration needed');
+        console.log('migration needed')
     }
 }
